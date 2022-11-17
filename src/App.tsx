47dceb241fb1a6e0365ea421af614a2a6753695a
Paulo011 <pauloortega011@gmail.com>
2022-11-17 01:32:15 -0300

    start repository

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {HeaderMain} from './components/header/header'
+
+function App() {
+  return(
+    <HeaderMain name={'The exchange rate today'}/>
+  )
+
+}
+
+export default App
